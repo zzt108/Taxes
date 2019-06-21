ca16d472c0c7fad625c56b2e049ebd05fe9df72a
zzt108 <zzt@outlook.hu>
2019-06-21 16:51:56 +0200

    Business logic integration tests successful

--- testController/ControllerTest.cs
+++ testController/ControllerTest.cs
@@ -16,5 +16,17 @@ namespace TestController
             Controller.Municipalities.GetMunicipalityId(context.Municipalities, "Vilnius").Should().Be(1);
             Controller.Municipalities.GetMunicipalityId(context.Municipalities, "vilnius").Should().Be(1);
         }
+
+
+        [TestMethod]
+        public void CanGetTaxValue()
+        {
+            var context = new ModelContext();
+            Controller.Taxes.GetTax(context.Municipalities, context.Taxes, "Vilnius", new DateTime(2016,1,1)).Should().Be(0.1f);
+            Controller.Taxes.GetTax(context.Municipalities, context.Taxes, "Vilnius", new DateTime(2016,5,2)).Should().Be(0.4f);
+            Controller.Taxes.GetTax(context.Municipalities, context.Taxes, "Vilnius", new DateTime(2016,7,10)).Should().Be(0.2f);
+            Controller.Taxes.GetTax(context.Municipalities, context.Taxes, "Vilnius", new DateTime(2016,3,16)).Should().Be(0.2f);
+        }
+
     }
 }
